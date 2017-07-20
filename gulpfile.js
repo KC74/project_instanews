6fbe4d3ec1410c8283f094f91134e6d23a62961d
Kenneth Chow <Ken@Kenneths-MacBook-Pro.local>
2017-07-20 10:39:32 -0700

    added eslint, uglify, rename, and browsersync.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,48 @@
-var gulp = require('gulp');
 
-gulp.task('default', function() {
-    // place code in here
-    console.log('Hello, world');
+// Required packages
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create(),
+    eslint = require('gulp-eslint');
+
+// scripts
+gulp.task('scripts', ['lint'], function() {
+    gulp.src('./js/*.js')
+    .pipe(uglify()) // call uglify function on files
+    .pipe(rename({ extname: '.min.js'})) // rename uglified file
+    .pipe(gulp.dest('./build/js')) // save to build/js folder
 });
+
+gulp.task('say_hello', function() {
+    console.log('hello!');
+}); 
+
+// watch scripts for changes and run script tasks
+gulp.task('watch', function () {
+    gulp.watch('./js/*.js', ['scripts']);
+}); // EOF watch
+
+
+// Reload Browser
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    }); // EOF browser sync
+
+    // watch the following for changes
+    gulp.watch('./build/js/*.js').on('change', browserSync.reload);
+});
+
+gulp.task('lint', function() {
+    return gulp.src(['./js/**/*.js','!./node_modules/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+// Gulp tasks
+gulp.task('default', ['watch', 'browser-sync']);
---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready( function() {
+
+alert('CYA NERD');
+
+
+}) // EOF doc.ready
